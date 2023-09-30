518eba9ff09917ecadd5c166eaa2f86a40058855
Spitfire <diego.velasco.bayon@gmail.com>
2023-09-30 18:43:59 +0200

    Fixed minor flake8 issue

--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -1,7 +1,19 @@
-""" This is a dummy example to show how to import code from src/ for testing"""
+""" This is a dummy example to show how to import code from src/ for testing """
 
-from src.module_1.module_1_meteo_api import main
+# from src.module_1.module_1_meteo_api import main
 
 
 def test_main():
+    raise NotImplementedError
+
+
+def test_request_with_backoff():
+    raise NotImplementedError
+
+
+def test_calculate_mean_stdev_of_models():
+    raise NotImplementedError
+
+
+def test_change_daily_2_yearly():
     raise NotImplementedError
