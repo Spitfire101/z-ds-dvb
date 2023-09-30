e3efd7564996469a84da023b07aeef312acc42b8
Spitfire101 <101530721+Spitfire101@users.noreply.github.com>
2023-09-30 19:00:17 +0200

    Update test_meteo_api.py

--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -3,17 +3,17 @@
 # from src.module_1.module_1_meteo_api import main
 
 
-def test_main():
-    raise NotImplementedError
+# def test_main():
+#    raise NotImplementedError
 
 
-def test_request_with_backoff():
-    raise NotImplementedError
+# def test_request_with_backoff():
+#    raise NotImplementedError
 
 
-def test_calculate_mean_stdev_of_models():
-    raise NotImplementedError
+# def test_calculate_mean_stdev_of_models():
+#    raise NotImplementedError
 
 
-def test_change_daily_2_yearly():
-    raise NotImplementedError
+# def test_change_daily_2_yearly():
+#    raise NotImplementedError
